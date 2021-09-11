d87f0a87ad6d04a4a88c4691c64916f0334b7d92
Jennie Dalgren <jennie.dalgren@gmail.com>
2021-09-11 21:09:09 +0200

    first

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+import config from 'config'
+import logger from './utils/logger'
+
+import socket from './socket'
+
+const port = config.get<number>('port')
+const host = config.get<string>('host')
+const corsOrigin = config.get<string>('corsOrigin')
+
+const app = express()
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true
+  }
+})
+
+app.get('/', (_, res) => res.send('SERVER IS UP!'))
+
+httpServer.listen(port, host, () => {
+  logger.info("🚀 server is listening 🎉")
+  logger.info(host + ' ' + port)
+
+  socket({io})
+})
---
+++ src/socket.ts
@@ -0,0 +1,69 @@
+import {Server, Socket} from 'socket.io'
+import logger from './utils/logger'
+
+const EVENTS = {
+  connection: 'connection',
+  CLIENT: {
+    CREATE_USER: 'CREATE_USER',
+    UPDATE_USER_POSITION: 'UPDATE_USER_POSITION',
+    SEND_MESSAGE: 'SEND_MESSAGE',
+  },
+  SERVER: {
+    USERS: 'USERS',
+    PULSE: 'PULSE'
+  },
+}
+
+const users: any[] = []
+
+
+function socket({io}: {io: Server}){
+  logger.info('sockets enabled')
+
+  setInterval(pulse, 1000);
+
+  function pulse() {
+    io.sockets.emit(EVENTS.SERVER.PULSE, users);
+  }
+
+  io.on(EVENTS.connection, (socket: Socket) => {
+    logger.info(`User connected with id ${socket.id}`)
+
+
+    // Get the list of users when site is loaded
+    socket.emit(EVENTS.SERVER.USERS, users)
+
+    /*
+    * When a user adds a username, a user gets created
+    */
+    socket.on(EVENTS.CLIENT.CREATE_USER, ({username, position}) => {
+      const user = {
+        id: socket.id,
+        name: username,
+        x: position.x,
+        y: position.y,
+        color: '#' + Math.floor(Math.random()*16777215).toString(16)
+      }
+      users.push(user)
+
+    })
+
+    /*
+    * When a user moves their mouse
+    */
+    socket.on(EVENTS.CLIENT.UPDATE_USER_POSITION, ({ position}) => {
+      users.map(user => {
+        if (user.id === socket.id) {
+          user.x = position.x
+          user.y = position.y
+        }
+        return user
+      })  
+    })
+
+    
+
+  })
+}
+
+export default socket
